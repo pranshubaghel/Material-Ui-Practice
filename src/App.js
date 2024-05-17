1379bc45534e88a8ba5cfaabb807e165b69eb6cc
pranshu baghel <devloperpranshu@gmail.com>
2024-05-17 12:52:37 +0530

    material ui

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// src/App.js
+
+import React from 'react';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h1" component="h2" gutterBottom>
+        Welcome to MUI
+      </Typography>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </Container>
+  );
+}
+
+export default App;
